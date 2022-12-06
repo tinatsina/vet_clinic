74d1be7af94253acd6407448c81f7dc73e5b99f1
Tinaye Tsinakwadi <tinayetsina@outlook.com>
2022-12-06 22:20:01 +0800

    update - review resolved

--- queries.sql
+++ queries.sql
@@ -3,9 +3,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 select * from animals where name like '%mon';
-select name from animals where '[2016-01-01,2019-01-01]'::daterange @> date_of_birth;
+select name from animals where '[2016-01-01,2019-12-31]'::daterange @> date_of_birth;
 select name from animals where neutered is true and escape_attempts < 3;
-select * from animals where name like '%Agumon' or name like '%Pikachu';
+select date_of_birth from animals where name like '%Agumon' or name like '%Pikachu';
 select name, escape_attempts from animals where weight_kg > 10.5;
 select * from animals where neutered is true;
 select * from animals where name not like '%Gabumon';
