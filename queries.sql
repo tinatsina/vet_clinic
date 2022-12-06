d9f2a84a7dfd39eacb302b64a149e00b0d534d33
Tinaye Tsinakwadi <tinayetsina@outlook.com>
2022-12-06 19:17:06 +0800

    update - added sql data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,13 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+/*Queries that provide answers to the questions from all projects.*/
+
+select * from animals where name like '%mon';
+select name from animals where '[2016-01-01,2019-01-01]'::daterange @> date_of_birth;
+select name from animals where neutered is true and escape_attempts < 3;
+select * from animals where name like '%Agumon' or name like '%Pikachu';
+select name, escape_attempts from animals where weight_kg > 10.5;
+select * from animals where neutered is true;
+select * from animals where name not like '%Gabumon';
+select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
+
